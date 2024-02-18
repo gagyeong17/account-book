432ba8c72c769a80c70714686ccca0a0dc5646ba
kimgagyeong <rkrud951117@naver.com>
2024-02-18 22:23:56 +0900

    로그인/회원가입페이지 퍼블리싱

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,12 @@
-const Button = () => {
-  return <button>Button</button>;
+interface ButtonProps {
+  children: React.ReactNode;
+}
+const Button = ({ children }: ButtonProps) => {
+  return (
+    <button className=" bg-sky-100 rounded-md w-full h-12 text-black hover:bg-sky-200 active:bg-sky-200 border-none">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
