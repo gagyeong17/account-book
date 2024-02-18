9d83d12dd00ce488da91d7bcf79deb5c4f20980a
kimgagyeong <rkrud951117@naver.com>
2024-02-18 17:59:33 +0900

    통계파트 퍼블리싱

---
+++ src/app/statistics/constants.ts
@@ -0,0 +1,1 @@
+export const BACKGROUND_COLOR = ['#ffeb9b', '#b5f2ff', '#c5f2ba'];
--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,5 +1,55 @@
+import Chart from './components/Chart';
+import SpendingList from './components/SpendingList';
+import { BACKGROUND_COLOR } from './constants';
 const Page = () => {
-  return <>통계</>;
+  const Data = {
+    labels: [
+      '입출금',
+      '증권',
+      '기타',
+      '입출금',
+      '증권',
+      '기타',
+      '입출금',
+      '증권',
+      '기타',
+    ], //api로 받아올 예정
+    datasets: [
+      {
+        data: [40, 20, 35, 40, 20, 35, 40, 20, 35], //api로 받아올 예정
+        backgroundColor: [
+          '#ffeb9b',
+          '#b5f2ff',
+          '#c5f2ba',
+          '#ffeb9b',
+          '#b5f2ff',
+          '#c5f2ba',
+          '#ffeb9b',
+          '#b5f2ff',
+          '#c5f2ba',
+        ],
+        borderColor: ['#ffeb9b', '#b5f2ff', '#c5f2ba'],
+      },
+    ],
+  };
+  const color = () => {
+    const colors = [];
+    for (let i = 0; i < Data.labels.length; i++) {
+      colors.push(BACKGROUND_COLOR[i % BACKGROUND_COLOR.length]);
+    }
+    return colors;
+  };
+  console.log(color());
+  const total = Data.datasets[0].data.reduce((acc, cur) => acc + cur, 0);
+  return (
+    <div className="w-full flex flex-col items-center">
+      <div className="my-4">
+        <span>지출: {total}원</span>
+      </div>
+      <Chart data={Data} />
+      <SpendingList data={Data} />
+    </div>
+  );
 };
 
 export default Page;
